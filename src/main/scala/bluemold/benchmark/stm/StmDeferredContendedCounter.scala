2dddefb9868a7a49cadb1e3ddabc447f169f8861
Neil Essy <neilessy@gmail.com>
2011-10-25 22:43:28 -0700

    Stm benchmark using deferred methods. Deferred operations are not executed until the transaction is attempting to complete using CASN. By using these methods one is able to provide better isolation for the computation and provide guarantees in some cases. Using deferred methods still maintains the composing properties of nested transactions provided by STM.

---
+++ src/main/scala/bluemold/benchmark/stm/StmDeferredContendedCounter.scala
@@ -0,0 +1,34 @@
+package bluemold.benchmark.stm
+
+import bluemold.stm._
+
+object StmDeferredContendedCounter {
+  val maxCounter = 10000000L
+  val counter = Ref( 0L )
+  def main( args: Array[String] ) {
+    val threadA = new Thread( new Counting )
+    val threadB = new Thread( new Counting )
+    val start = System.currentTimeMillis()
+    threadA.start()
+    threadB.start()
+    threadA.join()
+    threadB.join()
+    val end = System.currentTimeMillis()
+    val duration = end - start
+    System.out.println( "Duration: " + duration + "ms" )
+    System.out.println( "Transactions/Second: " + ( maxCounter * 1000 / duration ) )
+  }
+  class Counting extends Runnable {
+    def run() {
+      var currently = 0L
+      while ( currently < maxCounter ) {
+        atomicOn( counter ) {
+          deferredUpdate( counter )( _ + 1 )
+        } match {
+          case Some( value ) => currently = value
+          case None => // atomic failed, try again
+        }
+      }
+    }
+  }
+}
